665415e2471f6b76d86f5636e1a5befdd3f391ca
Agung Satria <123456789+agungsatria890@gmail.com>
2024-05-05 17:07:33 +0700

    shiftview + cycletag + keybind

--- config.h
+++ config.h
@@ -84,6 +84,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include <X11/XF86keysym.h>
+#include "movestack.c"
+// #include "cycletag.c"
+#include "shiftview.c"
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
@@ -105,6 +109,8 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define ALTKEY Mod1Mask
+#define XF86MonBrightnessDown 0x1008ff03
+#define XF86MonBrightnessUp 0x1008ff02
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -119,11 +125,21 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,				                XK_a,      spawn,          {.v = (const char*[]){ "rofi-dashboard", NULL } } },
+	{ MODKEY,			                  XK_d,		   spawn,          {.v = (const char*[]){ "rofi-run", NULL } } },
+	{ MODKEY,			                  XK_r,		   spawn,          {.v = (const char*[]){ "rofi-drun", NULL } } },
+	{ ALTKEY,			                  XK_space,	 spawn,          {.v = (const char*[]){ "rofi-drun-win", NULL } } },
+	// { MODKEY,			                  XK_g,		   spawn,          {.v = (const char*[]){ "rofi-todo", NULL } } },
+  { MODKEY,                       XK_g,      spawn,          SHCMD("rofi-todo; pkill -RTMIN+9 dwmblocks") },
+	// { MODKEY,				                XK_grave,	 spawn,          {.v = (const char*[]){ "rofi-emoji", NULL } } },
+	{ MODKEY,				                XK_grave,	 spawn,          {.v = (const char*[]){ "dmenuunicode", NULL } } },
+	{ ALTKEY,				                XK_Escape, spawn,          {.v = (const char*[]){ "rofi-powermenu", NULL } } },
+	{ MODKEY,				                XK_Escape, spawn,          {.v = (const char*[]){ "rofi-powermenu-big", NULL } } },
+	{ MODKEY,			                  XK_Return, spawn,          {.v = (const char*[]){ TERMINAL, NULL } } },
+	{ MODKEY|ControlMask,			      XK_Return, spawn,          {.v = (const char*[]){ TERMINAL, "-e", "tmux", "attach", NULL } } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY|ShiftMask,             XK_b,      togglesystray,  {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -137,6 +153,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
+	// { MODKEY,                       XK_o,      cycletag,       {.i = +1 } },
+	// { MODKEY,                       XK_i,      cycletag,       {.i = -1 } },
+	{ MODKEY,			                  XK_o,		   shiftview,	     { .i = +1 } },
+	{ MODKEY,			                  XK_i,		   shiftview,	     { .i = -1 } },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY|ControlMask,           XK_j,      defaultgaps,    {0} },
 	{ MODKEY|ControlMask,           XK_k,      togglegaps,     {0} },
@@ -200,6 +220,71 @@ static const Key keys[] = {
   { MODKEY,                       XK_slash,  togglescratch,  {.ui = 4 } },
 	{ ALTKEY,             		      XK_Tab,    altTabStart,	   {.i = 1} },
 	{ ALTKEY|ShiftMask,             XK_Tab,    altTabStart,	   {.i = 0} },
+
+	{ MODKEY,			        XK_w,		  spawn,     {.v = (const char*[]){ "firefox", NULL } } },
+	{ MODKEY|ShiftMask,   XK_w,		  spawn,     {.v = (const char*[]){ "firefox", "--private-window", NULL } } },
+	{ MODKEY|ShiftMask,   XK_e,		  spawn,     {.v = (const char*[]){ "nautilus", NULL } } },
+	{ MODKEY,			    XK_BackSpace,	spawn,     {.v = (const char*[]){ "i3lock", "-c", "000000", NULL } } },
+	{ MODKEY,			        XK_x,		  spawn,     {.v = (const char*[]){ "i3lock", "-c", "000000", NULL } } },
+	{ MODKEY,			        XK_v,		  spawn,     {.v = (const char*[]){ "clipmenu", NULL } } },
+  { MODKEY,             XK_y,     spawn,     SHCMD("gpick -pso --no-newline | xclip -sel c") },
+  { 0, XF86XK_AudioMute,          spawn,     SHCMD("changevolume mute; pkill -RTMIN+10 dwmblocks") },
+  { 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("changevolume down; pkill -RTMIN+10 dwmblocks") },
+  { 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("changevolume up; pkill -RTMIN+10 dwmblocks") },
+  { MODKEY,		          XK_F1,    spawn,     SHCMD("changemicvolume mute; pkill -RTMIN+12 dwmblocks") },
+  { MODKEY,		          XK_F2,    spawn,     SHCMD("changemicvolume down; pkill -RTMIN+12 dwmblocks") },
+  { MODKEY,		          XK_F3,    spawn,     SHCMD("changemicvolume up; pkill -RTMIN+12 dwmblocks") },
+  { 0, XF86MonBrightnessDown,     spawn,     {.v = (const char*[]){ "changebrightness", "down", NULL } } },
+  { 0, XF86MonBrightnessUp,       spawn,     {.v = (const char*[]){ "changebrightness", "up", NULL } } },
+
+  { 0, XF86XK_TouchpadToggle,     spawn,     {.v = (const char*[]){ "touchpad-toggle-x", NULL } } },
+
+  { MODKEY,             XK_F10,   spawn,     SHCMD("changevolume mute; pkill -RTMIN+10 dwmblocks") },
+  { MODKEY,             XK_F11,   spawn,     SHCMD("changevolume down; pkill -RTMIN+10 dwmblocks") },
+  { MODKEY,             XK_F12,   spawn,     SHCMD("changevolume up; pkill -RTMIN+10 dwmblocks") },
+  { MODKEY|ShiftMask,   XK_F11,   spawn,     {.v = (const char*[]){ "changebrightness", "down", NULL } } },
+  { MODKEY|ShiftMask,   XK_F12,   spawn,     {.v = (const char*[]){ "changebrightness", "up", NULL } } },
+
+  { MODKEY,			        XK_F9,		spawn,     SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+  { MODKEY,			        XK_F4,		spawn,     SHCMD(TERMINAL " -c float-st -e pulsemixer; pkill -RTMIN+10 dwmblocks && pkill -RTMIN+12 dwmblocks") },
+  { MODKEY,             XK_F7,		spawn,     {.v = (const char*[]){ "redshift-toggle", NULL } } },
+	// { MODKEY,				      XK_Print,	spawn,     SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
+  { MODKEY,             XK_c,		  spawn,     {.v = (const char*[]){ TERMINAL, "-c", "float-st", "-g", "100x25", "-e", "calcurse", NULL } } },
+  { MODKEY|ShiftMask,   XK_r,		  spawn,     {.v = (const char*[]){ TERMINAL, "-c", "float-st", "-g", "125x30", "-e", "btop", NULL } } },
+  { MODKEY|ALTKEY,      XK_n,		  spawn,     {.v = (const char*[]){ TERMINAL, "-c", "float-st", "-g", "100x25", "-e", "nmtui", NULL } } },
+  { 0,			            XK_Print, spawn,     {.v = (const char*[]){ "flameshot", "gui", NULL } } },
+	{ ShiftMask,			    XK_Print,	spawn,     {.v = (const char*[]){ "maimpick", NULL } } },
+	{ MODKEY,	            XK_Print,	spawn,     {.v = (const char*[]){ "scrshot", "full", NULL } } },
+	{ ControlMask,	      XK_Print,	spawn,     {.v = (const char*[]){ "scrshot", "full-copy", NULL } } },
+	{ MODKEY|ControlMask,	XK_p,	    spawn,     {.v = (const char*[]){ "scrshot", "window", NULL } } },
+	{ MODKEY|ShiftMask,	  XK_p,	    spawn,     {.v = (const char*[]){ "scrshot", "window-copy", NULL } } },
+	{ MODKEY|ControlMask,	XK_s,	    spawn,     {.v = (const char*[]){ "scrshot", "area", NULL } } },
+	{ MODKEY|ShiftMask,	  XK_s,	    spawn,     {.v = (const char*[]){ "scrshot", "area-copy", NULL } } },
+	{ ControlMask|ShiftMask,XK_Print,spawn,    {.v = (const char*[]){ "scrshot", "timer", NULL } } },
+	// { MODKEY,			        XK_g,	    spawn,     {.v = (const char*[]){ "dmenutodo", NULL } } },
+  { MODKEY,			        XK_z,	    spawn,     {.v = (const char*[]){ "dunstctl", "history-pop", NULL } } },
+  { MODKEY|ShiftMask,		XK_z,	    spawn,     {.v = (const char*[]){ "dunstctl", "close-all", NULL } } },
+  // { MODKEY,			                  XK_backslash,		   spawn,  {.v = (const char*[]){ "mpc", "toggle", NULL } } },
+  // { MODKEY|ShiftMask,	            XK_backslash,		   spawn,  {.v = (const char*[]){ "mpc", "stop", NULL } } },
+  // { MODKEY,			                  XK_bracketleft,	   spawn,  {.v = (const char*[]){ "mpc", "prev", NULL } } },
+  // { MODKEY,			                  XK_bracketright,   spawn,  {.v = (const char*[]){ "mpc", "next", NULL } } },
+  // { MODKEY|ShiftMask,			        XK_bracketleft,		 spawn,  {.v = (const char*[]){ "mpc", "seek", "-5", NULL } } },
+  // { MODKEY|ControlMask,		        XK_bracketleft,		 spawn,  {.v = (const char*[]){ "mpc", "seek", "-30", NULL } } },
+  // { MODKEY|ShiftMask,			        XK_bracketright,	 spawn,  {.v = (const char*[]){ "mpc", "seek", "+5", NULL } } },
+  // { MODKEY|ControlMask,		        XK_bracketright,	 spawn,  {.v = (const char*[]){ "mpc", "seek", "+30", NULL } } },
+  // { MODKEY|ControlMask|ShiftMask, XK_bracketleft,    spawn,  {.v = (const char*[]){ "mpc", "seek", "0%", NULL } } },
+  { MODKEY,			                  XK_backslash,		   spawn,  {.v = (const char*[]){ "mpc-util", "toggle", NULL } } },
+  { MODKEY|ShiftMask,	            XK_backslash,		   spawn,  {.v = (const char*[]){ "mpc-util", "stop", NULL } } },
+  { MODKEY,			                  XK_bracketleft,	   spawn,  {.v = (const char*[]){ "mpc-util", "prev", NULL } } },
+  { MODKEY,			                  XK_bracketright,   spawn,  {.v = (const char*[]){ "mpc-util", "next", NULL } } },
+	{ MODKEY|ShiftMask,			        XK_bracketleft,		 spawn,  {.v = (const char*[]){ "mpc-util", "seek5down", NULL } } },
+	{ MODKEY|ControlMask,		        XK_bracketleft,		 spawn,  {.v = (const char*[]){ "mpc-util", "seek30down", NULL } } },
+	{ MODKEY|ShiftMask,			        XK_bracketright,	 spawn,  {.v = (const char*[]){ "mpc-util", "seek5up", NULL } } },
+	{ MODKEY|ControlMask,		        XK_bracketright,	 spawn,  {.v = (const char*[]){ "mpc-util", "seek30up", NULL } } },
+  { MODKEY|ALTKEY,                XK_bracketleft,    spawn,  {.v = (const char*[]){ "mpc-util", "repeat", NULL } } },
+  { MODKEY|ALTKEY,                XK_bracketright,   spawn,  {.v = (const char*[]){ "mpc-util", "random", NULL } } },
+  { MODKEY|ControlMask|ShiftMask, XK_bracketleft,    spawn,  {.v = (const char*[]){ "mpc-util", "seek0", NULL } } },
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
