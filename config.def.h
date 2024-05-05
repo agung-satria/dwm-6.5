ed4848105f7fec4ce2b3e94ad3a07199fbdfbd40
Agung Satria <123456789+agungsatria890@gmail.com>
2024-05-05 17:25:56 +0700

    cycle layouts

--- config.def.h
+++ config.def.h
@@ -31,15 +31,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=13" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=13";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char normbgcolor[]       = "#141414";
+static const char normbordercolor[]   = "#272829";
+static const char normfgcolor[]       = "#abaeb5";
+static const char selfgcolor[]        = "#c7c9d0";
+static const char selbordercolor[]    = "#666a7c";
+static const char selbgcolor[]        = "#3c3f43";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor },
 };
 
 typedef struct {
@@ -122,8 +123,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
+// static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -188,6 +189,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[9]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[10]} },
 	{ MODKEY,                       XK_semicolon,setlayout,      {0} },
+	{ MODKEY|ControlMask,		        XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_f,      togglefullscreen, {0} },
   { MODKEY,			                  XK_s,		   togglesticky,	 {0} },
